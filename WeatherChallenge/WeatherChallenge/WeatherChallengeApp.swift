7c6c516c245cc2523919d5d7c0896e987c9c02d6
Jonashio <jonashio.es@gmail.com>
2024-08-15 23:22:12 +0200

    FIrst commit

---
+++ WeatherChallenge/WeatherChallenge/WeatherChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherChallengeApp.swift
+//  WeatherChallenge
+//
+//  Created by Jonashio on 15/8/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
